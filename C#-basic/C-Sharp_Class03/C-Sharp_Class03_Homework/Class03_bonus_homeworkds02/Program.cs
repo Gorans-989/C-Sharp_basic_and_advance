fe3cb702ddbd96969bfa283053cc3e2f82a8dfff
Gorans-989 <gorans989@yahoo.com>
2021-03-31 22:54:16 +0200

    0

---
+++ C#-basic/C-Sharp_Class03/C-Sharp_Class03_Homework/Class03_bonus_homeworkds02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class03_bonus_homeworkds02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
