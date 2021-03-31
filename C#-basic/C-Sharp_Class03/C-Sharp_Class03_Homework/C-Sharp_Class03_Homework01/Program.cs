a019fe80aeccae6a378a64a6de42bf19fb420a15
Gorans-989 <gorans989@yahoo.com>
2021-03-31 20:27:33 +0200

    Class03_homeworks

---
+++ C#-basic/C-Sharp_Class03/C-Sharp_Class03_Homework/C-Sharp_Class03_Homework01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace C_Sharp_Class03_Homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task01 SumOfSeven
+            //Task 01 - Sum of Seven
+            bool inputLength = false;
+
+            while (!inputLength)
+            {
+                Console.WriteLine("How many numbers you want in the array? type number: ");
+                inputLength = int.TryParse(Console.ReadLine(), out int lengthOfArray);
+
+                if (inputLength){
+                    int[] numbersArray = new int[lengthOfArray];
+                    int result01 = 0;
+                    int i = 0;
+
+                    while( i < lengthOfArray ){
+                        Console.WriteLine("Please enter number for position " + i);
+                        bool input = int.TryParse(Console.ReadLine(), out int number);
+                        if(input)
+                        {
+                            numbersArray[i] = number;
+                            Console.WriteLine("The number " + number + " is stored in the array at index: " + i);
+                            if(number % 2 == 0)
+                            {
+                               result01 += number;
+                            }
+                            i++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid number type. Please write only numbers!");
+                        }
+                    }
+                    Console.WriteLine("The sum of the even numbers from the array is :" + result01);
+                }
+                else {
+                    inputLength = false;
+                    Console.WriteLine("Wrong input. Please type in only numbers!");
+                }
+            }
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ C#-basic/C-Sharp_Class03/C-Sharp_Class03_Homework/C-Sharp_Class03_Homework02/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace C_Sharp_Class03_Homework02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 02
+            string[] studentGroup1 = { "goran", "zoran", "mile", "pero", "ace" };
+            string[] studentGroup2 = { "maja", "ana", "sanja", "tina", "iva" };
+
+            bool input = false;
+            while (!input)
+            {
+                Console.WriteLine("Please enter number '1' or number '2'");
+                input = int.TryParse(Console.ReadLine(), out int group);
+
+                if (input)
+                {
+                    if (group == 1 || group == 2)
+                    {
+                        switch(group)
+                        {
+                            case 1:
+                                Console.WriteLine("Student group 1 :");
+                                
+                                for (int i = 0; i < studentGroup1.Length; i++)
+                                {
+                                    Console.WriteLine("*" + studentGroup1[i]);
+                                }
+
+                                Console.WriteLine("=========== BYE :) ================");
+                                break;
+                            case 2:
+                                Console.WriteLine("Student group 2 :");
+                                
+                                for (int i = 0; i < studentGroup2.Length; i++)
+                                {    
+                                    Console.WriteLine("*" + studentGroup2[i]);   
+                                }
+
+                                Console.WriteLine("=========== BYE :) ================");
+                                break;
+                        }
+                    }
+                    else {
+                        input = false;
+                        Console.WriteLine("You entered group number: " + group + ". There is no such group"); 
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input type. Please type only numbers");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
